4ddc3aa0703411d6ee771d51d1c7492b0ed87767
Алексей Пожар <a.pozhar@x5.ru>
2024-09-13 09:43:19 +0300

    Добавил TODO

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ from mainapp.routers import main
 app = FastAPI()
 app.include_router(main.router)
 app.mount('/static/main', StaticFiles(directory='mainapp/static'), 'main_static')
+
+#TODO: переделай хранение проектов на посгрю
