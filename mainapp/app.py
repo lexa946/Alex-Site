c077a6f3e31d4eab91cbd6665503d6a3954a12f6
APozhar <apozhar@inno.tech>
2025-03-05 22:25:35 +0300

    Добавил мидлвэиры

--- mainapp/app.py
+++ mainapp/app.py
@@ -1,9 +1,17 @@
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
+from starlette.middleware.trustedhost import TrustedHostMiddleware
 
 from mainapp.routers.main import router as main_router
 
 app = FastAPI()
 app.mount('/static', StaticFiles(directory='mainapp/static'), 'main_static')
 app.include_router(main_router)
+
+app.add_middleware(HTTPSRedirectMiddleware)  # Перенаправляет HTTP на HTTPS
+app.add_middleware(
+    TrustedHostMiddleware,
+    allowed_hosts=["site.pozhar.keenetic.pro"]
+)
